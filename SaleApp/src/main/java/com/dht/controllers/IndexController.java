f8416c954d5733d3299dfab1a54ee58a119400fb
dhthanhqa@gmail.com <dhthanhqa@gmail.com>
2024-04-03 12:17:28 +0700

    update

---
+++ SaleApp/src/main/java/com/dht/controllers/IndexController.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.dht.controllers;
+
+import com.dht.services.CategoryService;
+import com.dht.services.ProductService;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ *
+ * @author admin
+ */
+@Controller
+public class IndexController {
+    @Autowired
+    private CategoryService cateService;
+    @Autowired
+    private ProductService prodService;
+    
+    @RequestMapping("/")
+    public String index(Model model,
+            @RequestParam Map<String, String> params) {
+        model.addAttribute("categories", this.cateService.getCates());
+        model.addAttribute("products", this.prodService.getProducts(params));
+        return "index";
+    }
+}
---
+++ SaleApp/target/generated-sources/annotations/com/dht/pojo/Tag_.java
@@ -0,0 +1,18 @@
+package com.dht.pojo;
+
+import com.dht.pojo.ProdTag;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SetAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2024-04-03T12:12:38")
+@StaticMetamodel(Tag.class)
+public class Tag_ { 
+
+    public static volatile SetAttribute<Tag, ProdTag> prodTagSet;
+    public static volatile SingularAttribute<Tag, String> name;
+    public static volatile SingularAttribute<Tag, Integer> id;
+    public static volatile SingularAttribute<Tag, String> tagcol;
+
+}
